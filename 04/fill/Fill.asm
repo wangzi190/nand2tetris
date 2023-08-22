3c5aaa950ba322f2644530e654d74095eb20d532
wangzi190 <gray.L.zhuang@gmail.com>
2023-08-22 14:10:38 -0700

    commit

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,8 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -11,4 +6,86 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(CHECK)
+    @24576
+    D=M // D = RAM[24576]
+
+    // Jump to (ERASE) if !(key pressed)
+    @ERASE
+    D;JEQ
+
+    D=1
+
+    (ERASE)
+    D=0
+
+    @131072
+    D=A
+    @counter
+    M=D // 256*512 = 131,072 pixels
+
+    @16384
+    D=A
+    @pixelNum
+    M=1 // Screen starts at RAM[16384]
+
+    @CHECK
+    0;JMP
+
+(DRAW)
+    D=1
+
+    (DRAWLOOP)
+        // blacken 1 pixel
+        @pixelNum
+        M=D // Memory[pixelNum] = 1
+        M=A+1 // pixelNum + 1
+
+        @counter
+        M=M-1 // Memory[counter] = Memory[counter] - 1
+        D=M // D = Memory[counter]
+
+        @CHECK
+        D;JLE
+
+        @DRAWLOOP
+        0;JMP
+
+    @CHECK
+    0;JMP // Loop back to (CHECK)
+
+(ERASE)
+    // Initialization
+
+    @131072
+    D=A
+    @counter
+    M=D // 256*512 = 131,072 pixels
+
+    @16384
+    D=A
+    @pixelNum
+    M=1 // Screen starts at RAM[16384]
+
+    D=0
+
+    @ERASELOOP
+    0;JMP
+
+   (ERASELOOP)
+        @pixelNum
+        M=D // Memory[pixelNum] = 0
+        M=A+1 // pixelNum + 1
+
+        @counter
+        M=M-1 // Memory[counter] = Memory[counter] - 1
+        D=M // D = Memory[counter]
+
+        @CHECK
+        D;JLE
+
+        @ERASELOOP
+        0;JMP
+
+    @CHECK
+    0;JMP // Loop back to (CHECK)
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -1,12 +1,50 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 //
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+
+// Initialize R2 to 0
+@R2
+M=0 // Memory[R2] = 0
+
+// Load R1 into D, then set counter to R1
+@R1
+D=M // D = Memory[R1]
+@counter
+MD=D // Memory[counter] = Memory[R1]
+
+//@END
+//D;JEQ
+// ^ ORIGINALLY DIDNT TAKE INTO ACCOUNT R1==0
+
+(ADD)
+    // Set counter to itself subtracted by 1
+    @counter
+    M=M-1 // Memory[counter] = Memory[counter] - 1
+
+    D=M // D = Memory[counter]
+    // ^ORIGINALLY FORGOT TO LOAD COUNTER INTO D
+
+    // Terminate program
+    @END
+    D;JLT // Jump to END if Memory[counter] <= 0
+
+    // Load R2 into D, then set R2 to itself plus R0
+    @R2
+    D=M // D = Memory[R2]
+    @R0
+    D=D+M // Memory[R2] = Memory[R2] + Memory[R0]
+    @R2
+    M=D //<--ORIGINALLY FORGOT TO PUT CALCULATION BACK INTO R2
+    // (BEFORE IT WAS ONLY IN REGISTER D)
+
+    // If not terminated, continue adding
+    @ADD
+    0;JMP // Jump to ADD if !(Memory[counter] <= 0)
+
+(END)
+    // Terminate program
+    @END
+    0;JMP
